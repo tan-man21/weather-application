49eb2e79157641ac2ed5100ef08ae3d000264441
Tanner Rogers <trroger2@alumni.ncsu.edu>
2026-02-04 18:02:40 -0500

    added initial search logic

---
+++ index.js
@@ -0,0 +1,33 @@
+const searchInput = document.getElementById('location-search');
+const temp = document.querySelector('.temp')
+const time = document.querySelector('.time')
+
+const getLocation = async () => {
+
+    let searchedName = searchInput.value
+
+    try {
+        const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchedName}`)
+        const data = await res.json();
+        
+        const latitude = data.results[0].latitude;
+        const longitude = data.results[0].longitude;
+        console.log(latitude, longitude)
+
+        const res2 = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&&current=temperature_2m,is_day&temperature_unit=fahrenheit`)
+        const data2 = await res2.json();
+        
+        console.log(data2)
+
+        temp.textContent = `${data2.current.temperature_2m}°F`;
+        if (data2.current.is_day === 1) {
+            time.textContent = '🌞'
+        } else {
+            time.textContent = '🌙'
+        }
+    } catch(e) {
+    console.log(e)
+  }
+}
+
+document.querySelector('button').addEventListener('click', getLocation)
